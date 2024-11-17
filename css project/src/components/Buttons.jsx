1412bb153b15f4d90717cb6d9d93b06602200ab2
EngineersWorld475 <sanjaygnair777@gmail.com>
2024-11-17 12:18:17 +0530

    css buttons

---
+++ css project/src/components/Buttons.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Buttons = () => {
+  return (
+    <div>
+      <button className='uber'>Request now</button>
+      <button className='amazon'>Add to cart</button>
+      <button className='netflix'>Get Started <span>&gt;</span></button>
+    </div>
+  )
+}
+
+export default Buttons
